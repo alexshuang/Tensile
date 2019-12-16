828fc896dc3f77c9df9618913dd072301fc51292
Alex Huang <nikshuang@sina.com>
2019-12-16 05:01:11 +0000

    optimize sp3_asm.sh

--- sp3/sp3_asm.sh
+++ sp3/sp3_asm.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/home/ramjana/ml_work/Tensile/mi100_work/bin
-/home/ramjana/ml_work/Tensile/mi100_work/bin/mi100_sp3 $1.sp3 asic=MI9 type=cs -hex $1.hex
-/home/ramjana/ml_work/Tensile/mi100_work/bin/mi100_sp3 -hex $1.hex asic=MI9 type=cs  $1_out.sp3
-cat $1_out.sp3 | grep "// " | grep ": " | sed "s/.*: //" | sed "s/ \([0-9a-f]*\)/, 0x\1/" |sed "s/^/.long 0x/" > $1.inc
-#/opt/rocm/opencl/bin/x86_64/clang -x assembler -target amdgcn-amdhsa -mcpu=gfx906 template.s -o output.co
+set -e
+
+export LD_LIBRARY_PATH=$PWD
+
+if [ "$#" -eq "0" ]; then
+	echo "Usage: sp3_asm.sh <source file|source dir>"
+	exit -1
+fi
+
+if [ -d $1 ]; then
+	SRC=`find $1 -name *.sp3`
+else
+	SRC=$1
+fi
+
+for o in $SRC; do
+	HEX=${o%.*}.hex
+	OUT=${o%.*}_out.sp3
+	INC=${o%.*}.inc
+	$PWD/mi100_sp3 $o asic=MI9 type=cs -hex $HEX
+	$PWD/mi100_sp3 -hex $HEX asic=MI9 type=cs  $OUT
+	cat $OUT | grep "// " | grep ": " | sed "s/.*: //" | sed "s/ \([0-9a-f]*\)/, 0x\1/" |sed "s/^/.long 0x/" > $INC
+	#/opt/rocm/opencl/bin/x86_64/clang -x assembler -target amdgcn-amdhsa -mcpu=gfx906 template.s -o output.co
+	echo
+	echo
+	echo "[Finish]: $o -> $INC"
+	echo
+	echo
+done
+
