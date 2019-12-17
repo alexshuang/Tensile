bbff62a4f23d0c9ce97fc52f54fbaca9b53f51e4
Alex Huang <nikshuang@sina.com>
2019-12-17 03:59:37 +0000

    new arch

--- sp3/sp3_asm.sh
+++ sp3/sp3_asm.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-export LD_LIBRARY_PATH=$PWD
+TOP_DIR=${RKT_DIR:-$PWD}
+BIN_DIR=$TOP_DIR/bin
+OUT_DIR=$TOP_DIR/out
+export LD_LIBRARY_PATH=$BIN_DIR
 
 if [ "$#" -eq "0" ]; then
 	echo "Usage: sp3_asm.sh <source file|source dir>"
@@ -15,7 +18,6 @@ else
 	SRC=$1
 fi
 
-OUT_DIR=out
 mkdir -p $OUT_DIR
 
 for o in $SRC; do
@@ -24,8 +26,8 @@ for o in $SRC; do
 	HEX=$BASENAME.hex
 	OUT=${BASENAME}_out.sp3
 	INC=$BASENAME.inc
-	$PWD/mi100_sp3 $o asic=MI9 type=cs -hex $HEX
-	$PWD/mi100_sp3 -hex $HEX asic=MI9 type=cs  $OUT
+	$BIN_DIR/mi100_sp3 $o asic=MI9 type=cs -hex $HEX
+	$BIN_DIR/mi100_sp3 -hex $HEX asic=MI9 type=cs  $OUT
 	cat $OUT | grep "// " | grep ": " | sed "s/.*: //" | sed "s/ \([0-9a-f]*\)/, 0x\1/" |sed "s/^/.long 0x/" > $INC
 	#/opt/rocm/opencl/bin/x86_64/clang -x assembler -target amdgcn-amdhsa -mcpu=gfx906 template.s -o output.co
 	echo
