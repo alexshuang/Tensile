218834eb070869010892a9fd6605419ac295a402
huang shuang <nikshuang@sina.com>
2020-11-02 10:06:52 +0000

    update dataset creation

--- ml_bench/dataset_create.py
+++ ml_bench/dataset_create.py
@@ -36,12 +36,6 @@ def df_create(features):
     df = pd.DataFrame()
     for k, v in features.items():
         df[k.strip()] = v
-
-    df.drop_duplicates(inplace=True)
-    df = df[~(df.GFlops < 0)].reset_index(drop=True)
-    df['TotalGFlop'] = df['TotalFlops'] / 1e9
-    df.drop('TotalFlops', axis=1, inplace=True)
-
     return df
 
 
@@ -54,41 +48,66 @@ def get_parameter_names(solutions:dict):
     return list(res)
 
 
-def dataset_create(basename:Path, test_pct=0.2, save_results=True):
+def add_feat(feat, solution):
+    for k, v in solution.items():
+        if k == 'ProblemType':
+            for _k, _v in v.items():
+                if isinstance(_v, list):
+                    for i, o in enumerate(_v):
+                        feat['PT_' + _k + f'_{i}'].append(o)
+                else:
+                    feat['PT_' + _k].append(strify(_v))
+        elif isinstance(v, list):
+            for i, o in enumerate(v):
+                feat[k + f'_{i}'].append(o)
+        else:
+            feat[k].append(strify(v))
+
+
+def dataset_create(basename:Path, test_pct=0.2, save_results=False, sampling_interval=1):
+    print(f"{basename} ...")
     df = pd.read_csv(basename.with_suffix('.csv'))
     sol_start_idx = 10
     solution_names = df.columns[sol_start_idx:]
     problem_size_names = df.columns[1:sol_start_idx]
     gflops = df.iloc[:, sol_start_idx:].values
-    rankings = gflops.argsort()
+    rankings = np.argsort(-gflops) # reverse
     num_solutions = len(solution_names)
 
     train_features, test_features = defaultdict(lambda: []), defaultdict(lambda: [])
     _solutions = yaml.safe_load(basename.with_suffix('.yaml').open())
     problem_sizes, solutions = df[problem_size_names].values, _solutions[2:]
     assert len(solution_names) == len(solutions)
-    col_set = set(get_parameter_names(solutions))
+    total_col_set = set(get_parameter_names(solutions)) # total feature names
 
     train_idxs, test_idxs = split_idxs(len(problem_sizes), test_pct)
+    assert(len(train_idxs) + len(test_idxs) == len(df))
 
 	# raw
-    for row, ps in enumerate(problem_sizes):
-        features = train_features if row in train_idxs else test_features
-        for col, (n, s) in enumerate(zip(solution_names, solutions)):
-            for k, v in zip(problem_size_names, ps):
+    for i, (problem_size, ranking) in enumerate(zip(problem_sizes, rankings)):
+        features = train_features if i in train_idxs else test_features
+
+        # train set can reduce sampling frequency
+        if i in train_idxs:
+            features = train_features
+            n = list(range(0, num_solutions, sampling_interval))
+        else:
+            features = test_features
+            n = list(range(num_solutions))
+
+        for j in n:
+            # problem sizes
+            for k, v in zip(problem_size_names, problem_size):
                 features[k.strip()].append(v)
-            for k, v in s.items():
-                if k == 'ProblemType':
-                    for _k, _v in v.items():
-                        features['PT_' + _k].append(strify(_v))
-                else:
-                    features[k].append(strify(v))
-            miss_cols = list(col_set - set(s.keys()))
-            for o in miss_cols:
-                features[o].append(np.nan)
-            features['SolutionName'].append(n)
-            features['GFlops'].append(gflops[row, col])
-            features['Ranking'].append((rankings[row, col] + 1) / num_solutions)
+            # solution features
+            idx = ranking[j]
+            solution, sname = solutions[idx], solution_names[idx]
+            add_feat(features, solution)
+            miss_cols = list(total_col_set - set(solution.keys()))
+            for o in miss_cols: features[o].append(np.nan)
+            features['SolutionName'].append(sname)
+            features['GFlops'].append(gflops[i, idx])
+            features['Ranking'].append(j / num_solutions)
 
     train_df = df_create(train_features)
     test_df = df_create(test_features)
@@ -114,24 +133,24 @@ if __name__ == '__main__':
         if os.path.exists(str(basename) + '.yaml'):
             src.append(basename)
 
-    # create train/test dataframe
-    #with ThreadPoolExecutor(os.cpu_count()) as e:
-    #    e.map(dataset_create, src)
     dfs, dfs2 = [], []
+    # create train/test dataframe
+#        with ThreadPoolExecutor(os.cpu_count()) as e:
+#            df, df2 = e.map(dataset_create, src)
+#            dfs.append(df)
+#            dfs2.append(df2)
     for o in src:
-        print("{} ...".format(o))
-        df, df2 = dataset_create(o, 0.2, False)
+        df, df2 = dataset_create(o, sampling_interval=2)
         dfs.append(df)
         dfs2.append(df2)
-        print("done")
 
     train_df = pd.concat(dfs, ignore_index=True)
-    train_df.to_csv(out/'train_raw.csv', index=False)
+    train_df.to_csv(out/'train_raw_interval_2.csv', index=False)
 
     del dfs, train_df
     
     test_df = pd.concat(dfs2, ignore_index=True)
-    test_df.to_csv(out/'test_raw.csv', index=False)
+    test_df.to_csv(out/'test_raw_interval_2.csv', index=False)
 
     end = time.time()
     print("Prepare data done in {} seconds.".format(end - start))
